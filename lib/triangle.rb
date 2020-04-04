6ed739a5663d5921428605fddc3a79621ac4ac46
Tom Banish <tombanish@gmail.com>
2020-04-04 00:34:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if (side_one == side_two) && (side_one == side_three) && (side_two == side_three)
       :equilateral
-    end
+    elsif side_one == 
   end
   
   
