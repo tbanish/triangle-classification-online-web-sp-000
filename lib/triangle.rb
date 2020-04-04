8f8427a2e3b8e1b692359269a4915c471ac76a2d
Tom Banish <tombanish@gmail.com>
2020-04-04 01:09:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,7 @@ class Triangle
     if (side_one == 0) && (side_two == 0) && (side_three == 0)
       begin
         raise TriangleError
+      rescue
     elsif (side_one == side_two) && (side_one == side_three) && (side_two == side_three)
       :equilateral
     elsif (side_two == side_three) || (side_one == side_three) || (side_one == side_two)
