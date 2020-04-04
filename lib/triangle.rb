a7350173e823eb4283c3d89fc98a088d3da69f76
Tom Banish <tombanish@gmail.com>
2020-04-04 01:18:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,8 @@ class Triangle
       raise TriangleError
     elsif (side_one < 0) || (side_two < 0) || (side_three < 0)
       raise TriangleError
-    elsif ((side_one + side_two) < side_three) || (()) || (())
+    elsif ((side_one + side_two) < side_three) || ((side_one + three) < side_two) || ((side_two + side_three) < side_one)
+      raise TriangleError
     elsif (side_one == side_two) && (side_one == side_three) && (side_two == side_three)
       :equilateral
     elsif (side_two == side_three) || (side_one == side_three) || (side_one == side_two)
