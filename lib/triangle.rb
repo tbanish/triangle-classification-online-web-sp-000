6ebebb7739fb72733cd003c2871635827c8fb865
Tom Banish <tombanish@gmail.com>
2020-04-04 01:10:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       begin
         raise TriangleError
       rescue TriangleError => error
-        
+        puts error.message
     elsif (side_one == side_two) && (side_one == side_three) && (side_two == side_three)
       :equilateral
     elsif (side_two == side_three) || (side_one == side_three) || (side_one == side_two)
